97d2c41469b00fa388c06e72272d240adc5fa855
Jordan Smith <cjordansmith@gmail.com>
2017-04-18 03:30:13 -0500

    completed section 3 lecture 16: Nested components

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,62 +1,73 @@
 // /public/app.jsx
-var Greeter = React.createClass({
 
-  // default properties when none are assigned
-  getDefaultProps: function() {
-    return {
-      name: 'React',
-      message: 'This is from a component!'
-    };
+var GreeterMessage = React.createClass({
+  render: function () {
+
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function (e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
   },
 
-  // on page load, assigns name variable to default
-  getInitialState: function() {
+  render: function () {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is from a component! Doug Heffernan.'
+    };
+  },
+  getInitialState: function () {
     return {
       name: this.props.name
     };
   },
-
-  // when 'Set Name' button clicked, change displayed name variable
-  onButtonClick: function(e) {
-    e.preventDefault();
-
-    var nameRef = this.refs.name;
-
-    // assigns variable name to input value
-    var name = nameRef.value;
-
-    // when button clicked, remove text from input field
-    nameRef.value = '';
-
-    if (typeof name === 'string' && name.length > 0) {
-      // updates state of name component from input
-      this.setState({
-        name: name
-      });
-    }
-
+  handleNewName: function (name) {
+    this.setState({
+      name: name
+    });
   },
-
-  render: function() {
+  render: function () {
     var name = this.state.name;
     var message = this.props.message;
 
     return(
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{message + '!!!'}</p>
-
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
+        <GreeterMessage name={name} message={message} />
+        <GreeterForm onNewName={this.handleNewName} />
       </div>
     );
   }
 });
 
 var firstName = 'Jordan';
-
 
 ReactDOM.render(
   <Greeter name={firstName}/>,
